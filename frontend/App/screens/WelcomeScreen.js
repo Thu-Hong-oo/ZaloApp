8ed69b479a7b1df3bfe64faf3c60169c528b432b
Ho Tram <21087811.tram@student.iuh.edu.vn>
2025-01-16 19:44:47 +0700

    welcome screen

---
+++ frontend/App/screens/WelcomeScreen.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View, Text, ScrollView, Image, TextInput, TouchableOpacity, StyleSheet, SafeAreaView } from 'react-native';
+import "@expo/metro-runtime";
+export default function WelcomeScreen({navigation, route}){
+    return (
+        <View style={{flex:1, backgroundColor:'#fff'}}>
+        <View style={{alignItems:'center', justifyContent:'center', height:'50%'}}>
+            <Text style={{fontWeight:'bold', color:'#0874fc', fontSize:80}}>ZALO</Text>
+        </View>
+        <View style={{alignItems:'center', justifyContent:'center', flex:1}}>
+            <TouchableOpacity style={{backgroundColor:'#186cfc', width:'80%', padding:15, marginBottom:10, borderRadius:25}} 
+            onPress={()=>{navigation.navigate('signIn')}}
+            >
+                <Text style={{color:'white', textAlign:'center', fontSize:18, fontWeight:500}}>Đăng nhập</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={{backgroundColor:'#f0ecec', padding:15, width:'80%', borderRadius:25}}>
+                <Text style={{textAlign:'center',  fontSize:18, fontWeight:500}}>Tạo tài khoản mới</Text>
+            </TouchableOpacity>
+        </View>
+        </View>
+    );
+}
