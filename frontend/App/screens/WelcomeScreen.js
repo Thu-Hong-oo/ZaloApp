0e612ed6c5ef07b38dc1666938080c23c3a1df54
Ho Tram <21087811.tram@student.iuh.edu.vn>
2025-01-16 22:09:27 +0700

    update giao dien

--- frontend/App/screens/WelcomeScreen.js
+++ frontend/App/screens/WelcomeScreen.js
@@ -9,11 +9,10 @@ export default function WelcomeScreen({navigation, route}){
         </View>
         <View style={{alignItems:'center', justifyContent:'center', flex:1}}>
             <TouchableOpacity style={{backgroundColor:'#186cfc', width:'80%', padding:15, marginBottom:10, borderRadius:25}} 
-            onPress={()=>{navigation.navigate('signIn')}}
             >
                 <Text style={{color:'white', textAlign:'center', fontSize:18, fontWeight:500}}>Đăng nhập</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={{backgroundColor:'#f0ecec', padding:15, width:'80%', borderRadius:25}}>
+            <TouchableOpacity style={{backgroundColor:'#f0ecec', padding:15, width:'80%', borderRadius:25}} onPress={()=>{navigation.navigate('signUp')}}>
                 <Text style={{textAlign:'center',  fontSize:18, fontWeight:500}}>Tạo tài khoản mới</Text>
             </TouchableOpacity>
         </View>
